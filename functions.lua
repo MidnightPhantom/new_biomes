55cfe5b6828b2eccbcd9e0b8cfb71c2e39330ffe
MidnightPhantom <96172170+MidnightPhantom@users.noreply.github.com>
2023-06-16 18:30:03 +0530

    Add files via upload

--- functions.lua
+++ functions.lua
@@ -68,52 +68,52 @@ minetest.register_abm({
  
 --
 -- For removing underground decorations if there is a block above them
---
+--This function is not needed anymore but still kept
 
-minetest.register_abm({
-               label = "decoration remove",
-               nodenames = {"group:underground_plantlife"},
-               interval = 2,
-               chance = 50,
-               catch_up = false,
-              action = function(pos, node)
-                              local above = {x = pos.x , y = pos.y + 1, z = pos.z}
-                              local abovet = {x = pos.x +1, y = pos.y, z = pos.z }
-                              local abovem = {x = pos.x -1, y = pos.y, z = pos.z}
-                              local abovec = {x = pos.x, y = pos.y, z = pos.z + 1}
-                              local aboveb = {x = pos.x, y = pos.y, z = pos.z - 1}
-                              local name = minetest.get_node(above).name
-                              local namet = minetest.get_node(abovet).name
-                              local namem = minetest.get_node(abovem).name
-                              local namec = minetest.get_node(abovec).name
-                              local nameb = minetest.get_node(aboveb).name
-                              local nodedef = minetest.registered_nodes[name]
-                              local nodedeft = minetest.registered_nodes[namet]
-                              local nodedefm = minetest.registered_nodes[namem]
-                              local nodedefc = minetest.registered_nodes[namec]
-                              local nodedefb = minetest.registered_nodes[nameb]
-                              if name ~= "ignore" and nodedef and not((nodedef.sunlight_propagates or
-                                                               nodedef.paramtype == "light") and
-                                                               nodedef.liquidtype == "none") then
-                               if namet ~= "ignore" and nodedeft and not((nodedeft.sunlight_propagates or
-                                                               nodedeft.paramtype == "light") and
-                                                               nodedeft.liquidtype == "none") then
-                                 if namem ~= "ignore" and nodedefm and not((nodedeft.sunlight_propagates or
-                                                               nodedefm.paramtype == "light") and
-                                                               nodedefm.liquidtype == "none") then
-                                   if namec ~= "ignore" and nodedefc and not((nodedef.sunlight_propagates or
-                                                               nodedefc.paramtype == "light") and
-                                                               nodedefc.liquidtype == "none") then
-                                    if nameb ~= "ignore" and nodedefb and not((nodedeft.sunlight_propagates or
-                                                               nodedefb.paramtype == "light") and
-                                                               nodedefb.liquidtype == "none") then
-                                                if node.name == "new_biomes:medium_pine" then
-                                                                 minetest.set_node(pos, {name = "new_biomes:glowing_dirt"})
-                                                end  
-                                      end
-                                    end
-                                  end
-                                end
-                               end
-              end
-})                                            
+--minetest.register_abm({
+              -- label = "decoration remove",
+               --nodenames = {"group:underground_plantlife"},
+               --interval = 2,
+               --chance = 50,
+               --catch_up = false,
+              --action = function(pos, node)
+                             -- local above = {x = pos.x , y = pos.y + 1, z = pos.z}
+                              --local abovet = {x = pos.x + 1, y = pos.y + 1, z = pos.z }
+                              --local abovem = {x = pos.x -1, y = pos.y + 1, z = pos.z}
+                              --local abovec = {x = pos.x, y = pos.y + 1, z = pos.z + 1}
+                              --local aboveb = {x = pos.x, y = pos.y + 1, z = pos.z - 1}
+                              --local name = minetest.get_node(above).name
+                              --local namet = minetest.get_node(abovet).name
+                              --local namem = minetest.get_node(abovem).name
+                              --local namec = minetest.get_node(abovec).name
+                              --local nameb = minetest.get_node(aboveb).name
+                              --local nodedef = minetest.registered_nodes[name]
+                              --local nodedeft = minetest.registered_nodes[namet]
+                              --local nodedefm = minetest.registered_nodes[namem]
+                              --local nodedefc = minetest.registered_nodes[namec]
+                              --local nodedefb = minetest.registered_nodes[nameb]
+                              --if name ~= "ignore" and nodedef and not((nodedef.sunlight_propagates or
+                                                               --nodedef.paramtype == "light") and
+                                                               --nodedef.liquidtype == "none") then
+                              -- if namet ~= "ignore" and nodedeft and not((nodedeft.sunlight_propagates or
+                                                               --nodedeft.paramtype == "light") and
+                                                               --nodedeft.liquidtype == "none") then
+                                 --if namem ~= "ignore" and nodedefm and not((nodedeft.sunlight_propagates or
+                                                               --nodedefm.paramtype == "light") and
+                                                               --nodedefm.liquidtype == "none") then
+                                   --if namec ~= "ignore" and nodedefc and not((nodedef.sunlight_propagates or
+                                                               --nodedefc.paramtype == "light") and
+                                                               --nodedefc.liquidtype == "none") then
+                                    --if nameb ~= "ignore" and nodedefb and not((nodedeft.sunlight_propagates or
+                                                               --nodedefb.paramtype == "light") and
+                                                               --nodedefb.liquidtype == "none") then
+                                                --if node.name == "new_biomes:medium_pine" then
+                                                                 --minetest.set_node(pos, {name = "new_biomes:glowing_dirt"})
+                                                --end  
+                                      --end
+                                    --end
+                                  --end
+                                --end
+                               --end
+              --end
+--})                                            
--- mapgen.lua
+++ mapgen.lua
@@ -1,93 +1,116 @@
 minetest.register_biome({
                name = "underground_grassland",
-               node_top = "new_biomes:gdwg",
-               depth_top = 3,
-               node_filler = "new_biomes:glowing_dirt",
-               depth_filler = 5,
                node_stone = "new_biomes:glowing_dirt",
                node_dungeon = "new_biomes:glowing_dirt",
                node_dungeon_stair = "new_biomes:gdwg",
                 y_max= -29000,
                 y_min = -31000,
+                vertical_blend = 10,
                 heat_point = 47,
-                humidity_point = 69,
+                humidity_point = 50,
 })
 
-minetest.register_ore({
-               ore_type = "scatter",
-               ore = "new_biomes:gdwg",
-               wherein = {"new_biomes:glowing_dirt"},
-               clust_scarcity = 3*3*3,
-               clust_num_ores = 70,
-               clust_size = 6,
-               y_min = -31000,
+minetest.register_decoration({
+                                name = "new_biomes:sgrassland_floor",
+		deco_type = "simple",
+		place_on = {"new_biomes:glowing_dirt"},
+		sidelen = 16,
+                                fill_ratio = 10,
+		biomes = {"underground_grassland"},
+		y_max = -29000,
+		y_min = -30910,
+		decoration = "new_biomes:gdwg",
+		place_offset = -1,
+		flags = "all_floors,force_placement",
+})
+
+minetest.register_biome({
+               name = "underground_sandstone_desert",
+               node_stone = "new_biomes:glowing_sandstone",
+               node_dungeon = "new_biomes:glowing_sandstone",
+               node_dungeon_stair = "default:sandstone",
                y_max = -29000,
-               biomes = {"underground_grassland"},
+               y_min = -30912,
+               vertical_blend = 10,
+               heat_point = 73,
+               humidity_point = 0,
+})
+
+minetest.register_decoration({
+                                name = "new_biomes:sdesert_floor",
+		deco_type = "simple",
+		place_on = {"new_biomes:glowing_sandstone"},
+		sidelen = 16,
+                                fill_ratio = 10,
+		biomes = {"underground_sandstone_desert"},
+		y_max = -29000,
+		y_min = -30910,
+		decoration = "new_biomes:glowing_sand",
+		place_offset = -1,
+		flags = "all_floors,force_placement",
 })
 
 minetest.register_biome({
                name = "underground_snowy_grassland",
-               node_top = "new_biomes:gdws",
-               depth_top = 3,
-               node_filler = "new_biomes:glowing_dirt",
-               depth_filler = 5,
                node_stone = "new_biomes:glowing_dirt",
                node_dungeon = "new_biomes:glowing_dirt",
                node_dungeon_stair = "new_biomes:gdws",
                 y_max= -29000,
                 y_min = -31000,
+                vertical_blend = 10,
                 heat_point = 0,
-                humidity_point = 69,
+                humidity_point = 65,
 })
 
 minetest.register_decoration({
+                                name = "new_biomes:snowy_grassland_floor",
 		deco_type = "simple",
 		place_on = {"new_biomes:glowing_dirt"},
-		sidelen = 4,
-		noise_params = {
-			offset = -0.004,
-			scale = 10,
-			spread = {x = 100, y = 100, z = 100},
-			seed = 53995,
-			octaves = 3,
-			persist = 1.0
-		},
+		sidelen = 16,
+                                fill_ratio = 10,
 		biomes = {"underground_snowy_grassland"},
 		y_max = -29000,
 		y_min = -30910,
 		decoration = "new_biomes:gdws",
-		place_offset_y = 0,
-		flags = "force_placement",
-	})
-
-minetest.register_ore({
-               ore_type = "scatter",
-               ore = "new_biomes:gdws",
-               wherein = {"new_biomes:gdwg"},
-               clust_scarcity = 3*3*3,
-               clust_num_ores = 70,
-               clust_size = 6,
-               y_min = -31000,
-               y_max = -29000,
-               biomes = {"underground_snowy_grassland"},
+		place_offset = -1,
+		flags = "all_floors,force_placement",
 })
 
 minetest.register_decoration({
 		deco_type = "simple",
 		place_on = {"new_biomes:gdws"},
 		sidelen = 16,
-		noise_params = {
-			offset = 0.8,
-			scale = 2,
-			spread = {x = 100, y = 100, z = 100},
-			seed = 137,
-			octaves = 3,
-			persist = 0.7
-		},
+                                fill_ratio = 0.9,
 		biomes = {"underground_snowy_grassland"},
 		y_max = -29000,
 		y_min = -30910,
 		decoration = "new_biomes:medium_pine",
-		place_offset_y = 0,
+		place_offset = 0,
 		flags = "force_placement",
-	})
+})
+
+minetest.register_biome({
+               name = "underground_glacier",
+               node_stone = "new_biomes:glowing_ice",
+               node_dungeon = "new_biomes:glowing_ice",
+               node_dungeon_stair = "new_biomes:glowing_snowblock",
+               vertical_blend = 10,
+                y_max= -29000,
+                y_min = -31000,
+                heat_point = 0,
+                humidity_point = 70,
+})
+
+minetest.register_decoration({
+                                name = "new_biomes:glacier_floor",
+		deco_type = "simple",
+		place_on = {"new_biomes:glowing_ice"},
+		sidelen = 16,
+                                fill_ratio = 10,
+		biomes = {"underground_glacier"},
+		y_max = -29000,
+		y_min = -30910,
+		decoration = "new_biomes:glowing_snowblock",
+		place_offset = -1,
+		flags = "all_floors,force_placement",
+})
