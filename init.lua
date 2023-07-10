f14eed633a4856b32e94a7d9acdfcf5b8bb6d7af
MidnightPhantom <96172170+MidnightPhantom@users.noreply.github.com>
2023-07-10 20:22:13 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,3 +4,4 @@ local modpath = minetest.get_modpath(minetest.get_current_modname());
 dofile (modpath.. "/functions.lua");
 dofile (modpath.. "/mapgen.lua");
 dofile (modpath.. "/nodes.lua");
+dofile (modpath.. "/plants.lua");
