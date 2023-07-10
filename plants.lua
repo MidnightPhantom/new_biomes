85fef1844789ceb205856d94f0aefbfee209e890
MidnightPhantom <96172170+MidnightPhantom@users.noreply.github.com>
2023-07-10 20:11:43 +0530

    Add files via upload

---
+++ plants.lua
@@ -0,0 +1,12 @@
+minetest.register_decoration({
+		deco_type = "simple",
+		place_on = {"new_biomes:gdws"},
+		sidelen = 16,
+                                fill_ratio = 0.2,
+		biomes = {"underground_snowy_grassland"},
+		y_max = -29000,
+		y_min = -30910,
+		decoration = "new_biomes:medium_pine",
+		place_offset = 0,
+		flags = "all_floors, force_placement",
+})
